7d5352a811e95ba7fb999eb1b98f72f4ec5ce25f
JoAnisky <aniskydev@protonmail.com>
2026-01-15 17:28:46 +0100

    [ADD] Custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            "ttt-white" : "#FCFAF9",
+            "ttt-black" : "#050505",
+            "ttt-grey-placeholders" : "#B8B8B8",
+            "ttt-dark-grey" : "#19191A",
+            "ttt-grey-stroke" : "#4C4C4C",
+            "ttt-status-red" : "#C40000",
+            "ttt-status-green" : "#0B6033",
+            "ttt-green-light" : "#677C2D",
+            "ttt-green-dark" : "#4F7A16",
+            "ttt-orange" : "#DD6835",
+        },
+    },
   },
   plugins: [],
 }
