9f18aaf2c2df9dd6b86b20b1a31b45f85f562b9f
JoAnisky <aniskydev@protonmail.com>
2026-01-15 14:53:44 +0100

    [ADD] Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    srcDir: 'app/',
-    devtools: { enabled: true },
-    vite: {
-        server: {
-            allowedHosts: [
-                process.env.DOMAIN || 'localhost',
-                'localhost',
-                '.local'
-            ]
-        }
-    }
+  srcDir: 'app/',
+  devtools: { enabled: true },
+
+  vite: {
+      server: {
+          allowedHosts: [
+              process.env.DOMAIN || 'localhost',
+              'localhost',
+              '.local'
+          ]
+      }
+  },
+
+  modules: ['@nuxtjs/tailwindcss']
 })
