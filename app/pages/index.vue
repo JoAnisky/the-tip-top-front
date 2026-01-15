5eeb9dd37a1132730b2fc78495e16307c2b3ef1f
JoAnisky <aniskydev@protonmail.com>
2026-01-15 17:59:12 +0100

    [ADD] first page !

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <h1>Hello CI/CD</h1>
+  <div>
+    <h1 class="mb-4 text-4xl font-bold tracking-tight text-heading md:text-5xl lg:text-6xl font-cream">Grand jeu concours Thé Tip Top</h1>
+  </div>
 </template>
