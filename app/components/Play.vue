c76412e7ebdc5c30f14ef6488f2211476a19e897
JoAnisky <aniskydev@protonmail.com>
2026-01-16 21:47:09 +0100

    [ADD] Header : Play btn & background image

---
+++ app/components/Play.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="bg-ttt-orange text-ttt-white px-8 py-3 rounded-full font-bold hover:scale-105 transition">
+    Jouer
+  </button>
+</template>
+
+<style scoped>
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <header>
-    <h1 class="main-title">Grand jeu concours Thé Tip Top</h1>
-    <p>Gagnez un an de thé bio premium d’une valeur de 360€ et de nombreux cadeaux !</p>
-    <button>Jouer</button>
-    <p>Pour fêter l’<strong>ouverture de notre 10<sup>e</sup> boutique Thé Tip Top à Nice</strong>, nous lançons un jeu concours unique ou chaque participation est 100% gagnante !</p>
+  <header class="w-full bg-[url('/images/hero-bg.jpg')] bg-cover bg-center py-20">
+    <div class="px-[var(--site-gutter)] text-ttt-white">
+      <h1 class="main-title">
+        Grand jeu concours Thé Tip Top
+      </h1>
+
+      <p class="text-xl mb-8 max-w-2xl font-lato">
+        Gagnez un an de thé bio premium d’une valeur de 360€ et de nombreux cadeaux !
+      </p>
+      <Play/>
+      <p class="mt-12 text-sm opacity-80 max-w-xl">
+        Pour fêter l’<strong>ouverture de notre 10<sup>e</sup> boutique Thé Tip Top à Nice</strong>, nous lançons un jeu concours unique ou chaque participation est 100% gagnante !
+      </p>
+    </div>
   </header>
 </template>
-<script setup lang="ts">
-</script>
