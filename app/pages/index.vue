7e0c87899a7444badd19d0b51647376a8ab3877a
JoAnisky <aniskydev@protonmail.com>
2026-01-16 08:24:49 +0100

    [TEST] CI/CD (optimization) force redeploy new pod version & remove Docker image after deploy

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1 class="mb-4 text-4xl font-bold tracking-tight text-heading md:text-5xl lg:text-6xl font-cream">Grand jeu concours Thé Tip Top</h1>
+    <p>Bienvenue dans l'univers Thé Tip Top !</p>
   </div>
 </template>
