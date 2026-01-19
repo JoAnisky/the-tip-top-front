ce44b67d88e7e864dfafa2999eaa3e8977b87fd0
JoAnisky <aniskydev@protonmail.com>
2026-01-19 18:14:09 +0100

    [UPGRADE] nuxt v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    future: {compatibilityVersion: 4},
     css: ['~/assets/scss/main.scss'],
     modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
     vite: {
