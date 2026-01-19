fed6a06e61c294a50b2e17de00ce32bec9c4e05d
JoAnisky <aniskydev@protonmail.com>
2026-01-19 14:31:19 +0100

    [ADD] Header : background image .web & responsive

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,38 @@
 <template>
-  <header class="w-full bg-[url('/images/hero-bg.jpg')] bg-cover bg-center py-20">
-    <div class="px-[var(--site-gutter)] text-ttt-white">
-      <h1 class="main-title">
-        Grand jeu concours Thé Tip Top
-      </h1>
+  <header class="relative w-full py-20">
+    <picture class="absolute inset-0">
+
+    <source media="(min-width: 1024px)" srcset="/images/hero-bg-1920.webp">
+      <source media="(min-width: 640px)" srcset="/images/hero-bg-1024.webp">
+      <img
+          src="/images/hero-bg-640.webp"
+          alt=""
+          class="absolute inset-0 w-full h-full object-cover"
+          loading="eager"
+          fetchpriority="high"
+          aria-hidden="true"
+      />
+    </picture>
+    <!-- Overlay -->
+    <div class="absolute inset-0 bg-black/60 z-0"></div>
+
+    <!-- Contenu -->
+    <div class="relative z-10 px-[var(--site-gutter)] text-ttt-white">
+      <h1 class="main-title">Grand jeu concours Thé Tip Top</h1>
 
       <p class="text-xl mb-8 max-w-2xl font-lato">
         Gagnez un an de thé bio premium d’une valeur de 360€ et de nombreux cadeaux !
       </p>
+
       <Play/>
+
       <p class="mt-12 text-sm opacity-80 max-w-xl">
-        Pour fêter l’<strong>ouverture de notre 10<sup>e</sup> boutique Thé Tip Top à Nice</strong>, nous lançons un jeu concours unique ou chaque participation est 100% gagnante !
+        Pour fêter l’<strong>ouverture de notre 10<sup>e</sup> boutique Thé Tip Top à Nice</strong>,
+        nous lançons un jeu concours unique où chaque participation est 100% gagnante !
       </p>
     </div>
   </header>
+
 </template>
+<script setup lang="ts">
+</script>
