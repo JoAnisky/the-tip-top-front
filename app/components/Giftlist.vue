d56ba8781a3f3ad64f9b4733cfebb19f6b2e74d9
JoAnisky <aniskydev@protonmail.com>
2026-01-20 12:08:34 +0100

    [ADD] Gift list component

---
+++ app/components/Giftlist.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface ImageSources {
+  mobile: string;
+  tablet: string;
+  desktop: string;
+}
+
+interface Gift {
+  name: string;
+  images: ImageSources;
+}
+
+const gifts: Gift[] = [
+  {
+    name: "Infuseur à thé",
+    image: "/images/gifts/infuseur-a-the.webp",
+  },
+  {
+    name: "Boîte de 100g Thé détox ou infusion",
+    image:  "/images/gifts/boite-the-ou-infusion.webp",
+  },
+  {
+    name: "Boîte de 100g Thé signature",
+    image: "/images/gifts/boite-the-signature.webp",
+  },
+  {
+    name: "Coffret découverte (valeur de 39€ ou 69€)",
+    image: "/images/gifts/coffret-decouverte.webp",
+  },
+];
+</script>
+
+<template>
+  <div v-for="(gift, index) in gifts" :key="index" class="group flex flex-col items-center max-w-[280px]">
+    <img :src="gift.image" :alt="gift.name" class="w-full h-auto object-contain transition-transform group-hover:scale-105" loading="lazy"/>
+      <p class="mt-4 text-center text-xl group-hover:text-ttt-orange">
+        {{ gift.name }}
+      </p>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,20 @@
-<template>
-  <AppHero/>
-
-</template>
-
 <script setup lang="ts">
 
 </script>
+
+<template>
+  <AppHero/>
+  <section id="gift-list">
+    <div class="ttt-container">
+      <div class="flex">
+        <h2>Fêtez l’ouverture de notre 10ᵉ boutique en remportant des lots exclusifs !</h2>
+      </div>
+      <div class="flex flex-wrap justify-center gap-8 mb-10">
+        <Giftlist/>
+      </div>
+      <div class="flex">
+        <p>Tentez de remporter ces cadeaux uniques et transformez votre expérience du thé.</p>
+      </div>
+    </div>
+  </section>
+</template>
