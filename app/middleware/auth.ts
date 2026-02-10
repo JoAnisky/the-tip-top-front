d6113e75900df98e8c092981dfaf3a08194bd864
Jonathan Loré <aniskydev@protonmail.com>
2026-02-10 17:07:39 +0100

    [UPDATE] Middleware enhancement

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,5 +1,10 @@
-export default defineNuxtRouteMiddleware(async () => {
-    const { loggedIn } = useUserSession();
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const { loggedIn, fetch } = useUserSession()
+
+    // en cas de doute, on force un rafraîchissement côté serveur/client
+    if (!loggedIn.value) {
+        await fetch()
+    }
 
     if (!loggedIn.value) {
         return navigateTo('/login')
