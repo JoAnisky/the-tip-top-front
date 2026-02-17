f24189577f980897e5a6bbe0d533cbedbb0994c5
Jonathan Loré <aniskydev@protonmail.com>
2026-02-17 11:02:08 +0100

    [REFACTO] GameSection + ADD PrizeWheel Component

---
+++ app/components/profile/GameSection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import PrizeWheel from "~/components/profile/PrizeWheel.vue";
+</script>
+
+<template>
+  <section id="game" class="relative overflow-hidden bg-gradient-ttt-dark">
+    <div class="ttt-container">
+      <h2 class="text-4xl font-bold text-center mb-2">Saisissez votre code</h2>
+      <div class="flex subtitle relative z-10">
+        <p class="mb-8">
+          Et lancez la roue pour découvrir ce que vous avez gagné !
+        </p>
+      </div>
+      <PrizeWheel/>
+    </div>
+    <img src="/images/leaf.png" alt="" aria-hidden="true" class="absolute bottom-0 right-0 w-48 pointer-events-none"/>
+  </section>
+</template>
---
+++ app/components/profile/PrizeWheel.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h3>Coucou la roue</h3>
+</template>
+
+<style scoped>
+
+</style>
