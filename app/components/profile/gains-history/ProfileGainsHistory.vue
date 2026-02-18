a042daa37c37152a754f2ab42d71c4471a3b7cfb
Jonathan Loré <aniskydev@protonmail.com>
2026-02-18 14:28:59 +0100

    [ADD] ProfileGainsHistory component

---
+++ app/components/profile/gains-history/ProfileGainsHistory.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { useGifts } from '~/composables/useGifts'
+import type { UserGain } from '~/types/game'
+
+const props = defineProps<{
+  gains: UserGain[]
+}>()
+
+const { getGiftById } = useGifts()
+
+function formatDate(isoDate: string): string {
+  const date = new Date(isoDate)
+  return date.toLocaleDateString('fr-FR', { day: 'numeric', month: 'long' })
+}
+</script>
+
+<template>
+  <div class="gains-history">
+    <div class="gains-table">
+      <!-- Header -->
+      <div class="table-header">
+        <div class="col-gain">Gain/Date</div>
+        <div class="col-code">Code</div>
+        <div class="col-status">Statut</div>
+      </div>
+
+      <!-- Rows -->
+      <div v-for="gain in gains" :key="gain.id" class="table-row">
+        <!-- Colonne Gain/Date -->
+        <div class="col-gain">
+          <img v-if="getGiftById(gain.gainId)" :src="getGiftById(gain.gainId)!.image" :alt="gain.gainName" class="gain-image"/>
+          <div class="gain-info">
+            <p class="gain-name">{{ gain.gainName }}</p>
+            <p class="gain-date">{{ formatDate(gain.validatedOn) }}</p>
+          </div>
+        </div>
+
+        <!-- Colonne Code -->
+        <div class="col-code">
+          <span class="code-value">{{ gain.code }}</span>
+        </div>
+
+        <!-- Colonne Statut -->
+        <div class="col-status">
+          <span v-if="gain.isClaimed" class="status-badge status-claimed">
+            Récupéré
+          </span>
+          <span v-else class="status-badge status-pending">
+            A récupérer
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss" src="./ProfileGainsHistory.scss" />
