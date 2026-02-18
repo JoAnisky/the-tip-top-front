f11278695f72acf2b847b7ad71b9c474e78f118a
Jonathan Loré <aniskydev@protonmail.com>
2026-02-18 14:18:12 +0100

    [ADD] UserGain type

--- types/game.ts
+++ types/game.ts
@@ -1,3 +1,13 @@
+export interface UserGain {
+    id: number
+    code: string
+    gainName: string
+    gainId: number
+    validatedOn: string // format ISO date
+    isClaimed: boolean
+    claimedOn: string | null
+}
+
 export interface WinResult {
     gainLabel: string
     gainId: number
