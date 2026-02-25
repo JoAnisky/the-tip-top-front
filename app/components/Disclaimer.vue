b30041ca180ee1bb5ebc2c50effae69910f745ac
Jonathan Loré <aniskydev@protonmail.com>
2026-02-25 10:03:28 +0100

    [ADD] Disclaimer component

---
+++ app/components/Disclaimer.vue
@@ -0,0 +1,10 @@
+<template>
+  <UAlert
+      color="orange"
+      variant="outline"
+      icon="i-heroicons-exclamation-triangle"
+      title="Projet pédagogique"
+      description="Il s'agit d'un projet Master. Les sociétés Thé Tip Top et Furious Duck ci-après nommées sont fictives et servent uniquement au déroulé du projet."
+      class="rounded-xl"
+  />
+</template>
