cca0bd251d89c1ea75fb254da310ca40c6d1a218
Jonathan Loré <aniskydev@protonmail.com>
2026-02-27 15:36:52 +0100

    [ADD] Contact form : contact post nuxt BFF

---
+++ server/api/contact.post.ts
@@ -0,0 +1,18 @@
+import { contactSchema } from "~/utils/contact-schema";
+
+export default defineEventHandler(async (event: Event) => {
+    const config = useRuntimeConfig()
+    const body = await readBody(event)
+
+    const parsed = contactSchema.safeParse(body)
+    if(!parsed.success) {
+        throw createError({ statusCode: 400, message: 'Données invalides' })
+    }
+
+    await $fetch(`${config.apiBaseUrl}/api/contact`, {
+        method: 'POST',
+        body: parsed.data,
+    })
+
+    return { success : true}
+})
