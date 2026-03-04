03bd3969f5f5cb333751a8d235ec918e2ad5f088
Jonathan Loré <aniskydev@protonmail.com>
2026-03-04 17:04:36 +0100

    [FIX] Accessibility : 5 Redundant alternative textReference

--- app/components/Giftlist.vue
+++ app/components/Giftlist.vue
@@ -6,7 +6,7 @@ const { gifts } = useGifts()
 <template>
   <div v-for="(gift, index) in gifts" :key="index" class="group flex flex-col items-center max-w-[280px]">
     <div class="relative transition-all duration-300 rounded-full group-hover:shadow-[0_0_30px_#fff]">
-      <img :src="gift.image" :alt="gift.name" class="w-full h-auto object-contain transition-transform duration-300 group-hover:scale-105" loading="lazy"/>
+      <img :src="gift.image" alt="" class="w-full h-auto object-contain transition-transform duration-300 group-hover:scale-105" loading="lazy"/>
     </div>
     <p class="mt-4 text-center text-xl group-hover:text-ttt-orange">
       {{ gift.name }}
