1b28a39f98aa3c068caf3d07e71f1de968556d80
Jonathan Loré <aniskydev@protonmail.com>
2026-03-06 14:46:14 +0100

    [FIX] Hydratation mismatch : SEO improvement

--- app/components/PlayBtn.vue
+++ app/components/PlayBtn.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 const { playLink } = usePlayLink()
 
+const resolvedLink = ref('/login')
+
+onMounted(() => {
+  resolvedLink.value = playLink.value
+})
 </script>
 
 <template>
-  <NuxtLink
-      :to="playLink"
-      class="btn-primary"
-      aria-label="Participer au jeu concours Thé Tip Top"
-  >
-    Jouer
-  </NuxtLink>
+    <NuxtLink
+        :to="resolvedLink"
+        class="btn-primary"
+        aria-label="Participer au jeu concours Thé Tip Top"
+    >
+      Jouer
+    </NuxtLink>
 </template>
