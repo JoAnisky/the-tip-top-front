5ac11791299e38ae9edae5e673c998c660282473
Jonathan Loré <aniskydev@protonmail.com>
2026-03-06 15:01:55 +0100

    [ADD] Removed typed composable

--- app/composables/usePlayLink.ts
+++ app/composables/usePlayLink.ts
@@ -1,10 +1,10 @@
 export const usePlayLink = () => {
     const { loggedIn } = useAuth()
 
-    const playLink : string = computed(() => loggedIn.value ? '/profile' : '/login')
-    const profileLabel : string = computed(() => loggedIn.value ? 'Mon profil' : 'Mon compte')
+    const playLink = computed(() => loggedIn.value ? '/profile' : '/login')
+    const profileLabel = computed(() => loggedIn.value ? 'Mon profil' : 'Mon compte')
 
-    const goToPlay = () : void => {
+    const goToPlay = () => {
         navigateTo(playLink.value)
     }
 
