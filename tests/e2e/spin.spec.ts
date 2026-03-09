3f66a866cf5b9f7b184b4d0c2e8a7341f4baa2b6
Jonathan Loré <aniskydev@protonmail.com>
2026-03-09 11:09:09 +0100

    [TEST](Playwright) e2e : Login and /profile redirect step OK

--- tests/e2e/spin.spec.ts
+++ tests/e2e/spin.spec.ts
@@ -1,16 +1,41 @@
 import { test, expect } from '@playwright/test'
 
 test('login puis soumission d\'un code gagnant', async ({ page }) => {
+    // ------- POUR DEBUG  ------- \\
+    // page.on('request', req => {
+    //     if (req.url().includes('auth')) {
+    //         console.log('REQUEST:', req.method(), req.url())
+    //     }
+    // })
+    // page.on('response', async res => {
+    //     if (res.url().includes('auth')) {
+    //         console.log('RESPONSE:', res.status(), res.url())
+    //     }
+    // })
+    // page.on('response', async res => {
+    //     if (res.url().includes('auth/login')) {
+    //         const headers = res.headers()
+    //         console.log('LOGIN HEADERS:', JSON.stringify(headers, null, 2))
+    //     }
+    // })
+
     // Etape 1 : aller sur la page login
     await page.goto('/login')
 
+    // Fermer la bannière cookie en premier
+    await page.getByRole('button', { name: 'Tout refuser' }).click()
+
     // 2 : remplir le formulaire de login
-    await page.getByLabel('Email').fill(process.env.TEST_USER_EMAIL)
-    await page.getByLabel('Mot de passe').fill(process.env.TEST_USER_PASSWORD)
+    await page.getByRole('textbox', { name: 'Adresse email' }).click()
+    await page.keyboard.type(process.env.TEST_USER_EMAIL)
+
+    await page.getByRole('textbox', { name: 'Mot de passe' }).click()
+    await page.keyboard.type(process.env.TEST_USER_PASSWORD)
+
     await page.getByRole('button', { name: 'Se connecter' }).click()
 
-    // 3 : On vérfie qu'on est bien redirigé vers la page profil
-    await expect(page).toHaveUrl('/profile')
+    // 3 : On vérifie qu'on est bien redirigé vers la page profil
+    await page.waitForURL('/profile', { timeout: 8000 })
 
     // 4 : Soumettre un code gagnant
     await page.getByPlaceHolder('Code unique à 10 caractères').fill(process.env.TEST_WIN_CODE)
