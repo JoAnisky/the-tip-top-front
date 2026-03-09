e8a07fab5ad4309b04d8813856691d70a49a82a9
Jonathan Loré <aniskydev@protonmail.com>
2026-03-09 10:29:33 +0100

    [TEST](Playwright) : add chromium and .env.test resolve

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from '@playwright/test'
+import { defineConfig, devices } from '@playwright/test'
+import dotenv from 'dotenv'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+dotenv.config({ path: resolve(__dirname, '.env.test') })
 
 export default defineConfig({
     testDir: './tests/e2e',
     use: {
-        baseUrl: process.env.BASE_URL || 'http://the-tip-top.front.dev.local',
+        baseURL: process.env.BASE_URL || 'http://the-tip-top.front.dev.local',
+        launchOptions: {
+            executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
+        }
     }
 })
